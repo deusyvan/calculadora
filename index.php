7d250d74ec9fbaaa07d5cff3231f939db693575e
deusyvan <deusyvan@gmail.com>
2018-06-23 07:57:01 -0300

    Definindo nosso numero inicial

--- index.php
+++ index.php
@@ -2,28 +2,21 @@
 
 class Calculadora {
     
-    public function somar($n1, $n2){
-        return $n1 + $n2;
+    private $n;
+    
+    public function __construct($numeroInicial){
+        $this->n = $numeroInicial;
     }
     
-    public function subtrair($n1, $n2){
-        return $n1 - $n2;
-    }
-    
-    public function multiplicar($n1, $n2){
-        return $n1 * $n2;
-    }
-    
-    public function dividir($n1, $n2){
-        return $n1 / $n2;
-    }
+   
 }
 
-    $calc = new Calculadora();
+    $calc = new Calculadora(10);
 
-    $calc->definirNumeroInicial(10);
     $calc->somar(2)->subtrair(3)->multiplicar(5)->dividir(2);
-    $resultado = $calc->resultado();
+    $resultado = $calc->resultado(); //22.5
+    
+    
     
     
 ?>
